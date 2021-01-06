78262bbc9a873c56b60d5782d27d30c7d9269a28
wknobbe <wknobbe4@gmail.com>
2021-01-06 14:42:27 -0600

    Age button

--- propitup/src/components/MyMainComponent.js
+++ propitup/src/components/MyMainComponent.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class MyMainComponent extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+    handleAge = () => {
+        this.setState({age: this.state.age + 1});
+    }
     render(){
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <h3>Age: {this.props.age}</h3>
+                <h3>Age: {this.state.age}</h3>
                 <h3>Hair Color: {this.props.hairColor}</h3>
+                <button onClick={ this.handleAge }>Birthday button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
